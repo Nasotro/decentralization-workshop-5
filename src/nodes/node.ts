a2fa004138a838e6a690f0ce2d89151e3c422ecc
Nasotro <lorrain.morlet@edu.devinci.fr>
2025-03-25 19:32:23 +0100

    everything works

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -19,7 +19,7 @@ export async function node(
   let nodeState: NodeState = {
     killed: false,
     x: initialValue,
-    decided: false,
+    decided: isFaulty ? null : false,
     k: 0,
   };
 
@@ -31,6 +31,9 @@ export async function node(
       k: null,
     };
   }
+
+  let phase1Messages = Array(N).fill(null);
+  let phase2Messages = Array(N).fill(null);
 
   let receivedValues: (Value | null)[] = Array(N).fill(null);
   receivedValues[nodeId] = initialValue;
@@ -51,62 +54,123 @@ export async function node(
 
   // this route allows the node to receive messages from other nodes
   node.post("/message", (req, res) => {
-    const { senderId, value } = req.body;
-    if (senderId !== nodeId && !isFaulty) {
-      // Update the node's state based on the received message
-      receivedValues[senderId] = value;
+    if (nodeState.killed) {
+      res.status(500).send("Node is killed");
+      return;
+    }
+    if (!nodeState.decided) {
+      const { senderId, value } = req.body;
+      const phase = value.phase;
+      const k = value.k;
+      const x = value.x;
 
-      const n_received = receivedValues.filter((v) => v !== null).length;
-      
-      console.log(`Node ${nodeId} received message from node ${senderId} with value ${value} | recieved values: ${receivedValues}, check consensus ? ${n_received >= N - F - 1} (N: ${N}, F: ${F})`);
+      // if the node is faulty, it doesn't respond to messages
+      if (isFaulty) {
+        res.status(200).send("Message received by faulty node");
+        return;
+      }
 
-
-
-      // Check if consensus is reached
-      if (!nodeState.decided && nodeState.x !== null) {
-        let consensusCount = 0;
-        for (let i = 0; i < N; i++) {
-          if (receivedValues[i] === nodeState.x) {
-            consensusCount++;
-          }
+      if (nodeState.k === value.k) {
+        if (phase === 1) {
+          phase1Messages[senderId] = x;
         }
-        if (consensusCount >= (N - F)/2) {
-          nodeState.decided = true;
-          console.log(`Consensus reached by node ${nodeId} with value ${nodeState.x}`);
-        }
-        else{
-          console.log(`Consensus not reached by node ${nodeId} with value ${nodeState.x}`);
-          if (nodeState.k === 0) {
-            nodeState.k = 1; // Start the first round
-          }
-          if (nodeState.x === null) {
-            nodeState.x = value; // Initialize x if it's null
-          }
-          nodeState.x = Math.random() < 0.5 ? nodeState.x : value; // Randomly decide the value
+        if (phase === 2) {
+          phase2Messages[senderId] = x;
         }
       }
     }
+
     res.status(200).send("Message received");
   });
 
+  async function sendMessageToAll(phase: number, k: number, x: Value) {
+    for (let i = 0; i < N; i++) {
+      if (i !== nodeId) {
+        await fetch(`http://localhost:${BASE_NODE_PORT + i}/message`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ senderId: nodeId, value: { phase, k, x } }),
+        });
+      }
+    }
+  }
+
   // this route is used to start the consensus algorithm
   node.get("/start", async (req, res) => {
-    if (!isFaulty) {
-      // Send the initial value to all other nodes
+    if (isFaulty || nodeState.killed || nodeState.k == null || nodeState.x == null) {
+      res.status(500).send("Node is faulty or killed");
+      return;
+    }
+    while (!nodeState.decided && !nodeState.killed && nodeState.k! <= 12) {
+      nodeState.k++;
+
+      // PHASE 1:
+      await sendMessageToAll(1, nodeState.k, nodeState.x);
+      console.log(`Node ${nodeId} sent phase 1 messages`);
+      //get the majority value of the phase 1 messages
+      let majorityValue1: Value = 0;
+      let majorityValues1 = [0, 0];
+
       for (let i = 0; i < N; i++) {
-        if (i !== nodeId) {
-          await fetch(`http://localhost:${BASE_NODE_PORT + i}/message`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ senderId: nodeId, value: initialValue }),
-          });
+        if (phase1Messages[i] !== null) {
+          majorityValues1[phase1Messages[i]]++;
         }
       }
-      // Begin the decision-making process
-      nodeState.k = 1; // Advance to the next round
-      console.log(`Consensus started by node ${nodeId}`);
+      if (majorityValues1[0] > (N - F) / 2) {
+        // if (majorityValues1[0] > N / 2) {
+        majorityValue1 = 0;
+      }
+      // else if (majorityValues1[1] > N / 2) {
+      else if (majorityValues1[1] > (N - F) / 2) {
+        majorityValue1 = 1;
+      }
+      else {
+        majorityValue1 = 1;
+      }
+      console.log(`Node ${nodeId} decided on value ${majorityValue1} for the phase 1`);
+
+
+      // PHASE 2:
+      await sendMessageToAll(2, nodeState.k, majorityValue1);
+      console.log(`Node ${nodeId} sent phase 2 messages`);
+
+      //get the majority value of the phase 2 messages
+      let majorityValue2: Value = 0;
+      let majorityValues2 = [0, 0];
+
+      for (let i = 0; i < N; i++) {
+        if (phase2Messages[i] !== null) {
+          majorityValues2[phase2Messages[i]]++;
+        }
+      }
+
+      // console.log(`Node ${nodeId} F is ${F}, N is ${N}, F*2 is ${F * 2}`);
+
+      if ((F * 2) >= N) { // if we exceed the fault limit
+        console.log(`Node ${nodeId} exceeded the fault limit`);
+        nodeState.x = Math.floor(Math.random() * 2) as Value;
+        // nodeState.decided = false;
+      }
+      else {
+        // if (majorityValues2[0] >= F + 1) {
+        if (majorityValues2[0] > (N - F) / 2) {
+          majorityValue2 = 0;
+        }
+        else if (majorityValues2[1] > (N - F) / 2) {
+          majorityValue2 = 1;
+        }
+        else {
+          majorityValue2 = majorityValue1;
+        }
+        nodeState.x = majorityValue2;
+        nodeState.decided = true;
+        // nodeState.k++;
+      }
+      console.log(`Node ${nodeId} decided on value ${majorityValue2} for the phase 2`);
+      // console.log(`state of node ${nodeId} is ${JSON.stringify(nodeState)}`);
+      
     }
     res.status(200).send("Consensus started");
   });
@@ -120,9 +184,9 @@ export async function node(
 
   // start the server
   const server = node.listen(BASE_NODE_PORT + nodeId, async () => {
-    console.log(
-      `Node ${nodeId} is listening on port ${BASE_NODE_PORT + nodeId}`
-    );
+    // console.log(
+    //   `Node ${nodeId} is listening on port ${BASE_NODE_PORT + nodeId}`
+    // );
 
     // the node is ready
     setNodeIsReady(nodeId);
